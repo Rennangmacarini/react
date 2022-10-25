642eeb332e1b420a5ad6314c61c8595b8abdf902
Rennan Macarini <rennangmacarini@gmail.com>
2022-10-25 09:54:01 -0300

    atualizando

---
+++ ignews/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return [ 
+    <> 
+    <Head>
+    <title>Inicio | ig.news {}</title>
+    <meta name="description" content=""/>
+    </Head>
+    <h1>
+      Hello World
+    </h1>
+    </>
+  ]
+}
